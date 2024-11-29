a78f57ddbea9691de1dcb3a6bcc887b631d958a3
Jules SCHATTNER <jschattner@guardiaschool.fr>
2024-11-29 13:01:22 +0100

    dash update

--- assets/js/java dash.js
+++ assets/js/java dash.js
@@ -1,3 +1,22 @@
+const menuIcon = document.querySelector('.menu-icon');
+const sideBar = document.querySelector('#sidebar');
+const header = document.querySelector('.header');
+
+menuIcon.addEventListener('click', toggleSidebar);
+
+function toogleSidebar() {
+    if (sidebar.classList.contains('sidebar-responsive')) {
+        sidebar.classList.remove('sidebar-responsive');
+        menuIcon.querySelector('span').innerText = 
+            'keyboard_double_arrow_right';
+        header.classList.remove('header-responsive');
+    } else {
+        sidebar.classList.add('sidebar-responsive');
+        menuIcon.querySelector('span').innerText = 'menu';
+        header.classList.add('header-responsive');
+    }
+}
+
 let options = {
     series: [
         {
@@ -9,7 +28,7 @@ let options = {
         height: 350,
         toolbar: {
             show: false,
-        }
+        },
     },
   colors : ['#eba91d', '#bc1e51', '#04a07e', '434738', '#0b0c0e'],
   plotOptions: {
@@ -21,21 +40,14 @@ let options = {
         },  
     },
     dataLabels: {
-        enabled: false
+        enabled: false,
   },
   legend: {
-    show: false
+        show: false,
   },
   xaxis: {
-    categories: [
-            'Pierre', 
-            'David', 
-            'Jules', 
-            'Alexia', 
-            'Max', 
-            
-        ],
-     },
+        categories: ['Pierre', 'David',  'Jules', 'Alexia', 'Max'],
+     }, 
      yaxix: {
         title: {
             text: "Recommandations",
