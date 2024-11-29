2b8e1c1ac6a2bbe71f83fcf73250ee8e7f830c09
Pierre <pierre@Ordinateur-portable-de-Pierre.local>
2024-11-29 12:36:14 +0100

    contact

--- index.php
+++ index.php
@@ -68,7 +68,38 @@
 							<p class="copyright"> </p>
 						</footer>
 
+
+						<article id="contact">
+								<h2 class="major">Contactez moi !</h2>
+								<form method="post" action="send-email.php">
+									<div class="fields">
+										<div class="field half">
+											<label for="name">Nom</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field half">
+											<label for="subject">Objet</label>
+											<input type="text" name="subject" id="subject" />
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="4"></textarea>
+										</div>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Envoyer" class="primary" /></li>
+										<li><input type="reset" value="Annuler" /></li>
+									</ul>
+								</form>
+							</article>
+
 			</div>
+
+			
 
 		<!-- BG -->
 			<div id="bg"></div>
